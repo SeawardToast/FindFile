aa8d1efe086b4d60857eb3adb3ace081747cd4cc
Aidan <aidan@murphyhouse.us>
2019-12-09 21:57:12 -0500

    finished

--- FileSearch/Program.cs
+++ FileSearch/Program.cs
@@ -8,7 +8,16 @@ namespace FileSearch
         {
             Console.WriteLine("Hello World!");
             SearchUtils util = new SearchUtils();
-            util.FindFile("hello.txt", @"C:\Users\aidan\Documents");
+            string file = string.Empty;
+            string path = string.Empty;
+            string extension = "*";
+            Console.WriteLine("What is the name of the file you are searching for? (include extension)");
+            file = Console.ReadLine();
+            extension += file.Split('.')[1];
+            Console.WriteLine("Which directory should we search for this file?");
+            path = Console.ReadLine();
+            util.FindFile(file, path, extension);
+            
         }
     }
 
@@ -19,21 +28,28 @@ namespace FileSearch
 
         }
 
-        public void FindFile(string fileName, string directory)
+        public void FindFile(string fileName, string directory, string extension)
         {
+            bool flag = false;
             try
             {
-                foreach (string file in Directory.GetFiles(directory))
+                string[] files = Directory.GetFiles(directory, extension, SearchOption.AllDirectories);
+                foreach (string file in files)
                 {
-                    
-                    if(Path.GetFileName(file).Equals(fileName))
-                        Console.WriteLine("Your file has been found at location " + file);
+
+                    if (Path.GetFileName(file).Equals(fileName))
+                    {
+                        Console.WriteLine("We have found the file at " + file);
+                        flag = true;
+                    }
+                        
                 }
             } catch(UnauthorizedAccessException e)
             {
                 
             }
-            
+            if(!flag)
+                Console.WriteLine("We have not found the file");
         }
     }
 }
