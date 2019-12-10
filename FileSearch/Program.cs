ea82128b4fa4614b4b9fe30fa053c6aaadc82716
Aidan <aidan@murphyhouse.us>
2019-12-09 19:43:16 -0500

    Add project files.

---
+++ FileSearch/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+namespace FileSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            SearchUtils util = new SearchUtils();
+            util.FindFile("hello.txt", @"C:\Users\aidan\Documents");
+        }
+    }
+
+    class SearchUtils
+    {
+        public SearchUtils()
+        {
+
+        }
+
+        public void FindFile(string fileName, string directory)
+        {
+            try
+            {
+                foreach (string file in Directory.GetFiles(directory))
+                {
+                    
+                    if(Path.GetFileName(file).Equals(fileName))
+                        Console.WriteLine("Your file has been found at location " + file);
+                }
+            } catch(UnauthorizedAccessException e)
+            {
+                
+            }
+            
+        }
+    }
+}
